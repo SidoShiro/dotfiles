a60333b6526c3f85b5019fa4a532f402855ea30a
sidore_m <marc.sidorenko@epita.fr>
2018-04-11 09:01:23 +0200

    Stuff

---
+++ vim/bundle/shell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone https://github.com/eagletmt/ghcmod-vim.git
+ git clone https://github.com/eagletmt/neco-ghc
+ git clone https://github.com/ctrlpvim/ctrlp.vim.git
+ git clone https://github.com/scrooloose/syntastic.git
+ git clone https://github.com/tomtom/tlib_vim.git
+ git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+ git clone https://github.com/garbas/vim-snipmate.git
+ git clone https://github.com/scrooloose/nerdtree.git
+ git clone https://github.com/scrooloose/nerdcommenter.git
+ git clone https://github.com/godlygeek/tabular.git
+ git clone https://github.com/ervandew/supertab.git
+ git clone https://github.com/Shougo/neocomplete.vim.git
